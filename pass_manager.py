b083e75be17c310835aa5082b83c22c096540006
Sharon-Rose Mercy <mercysharonrose02@gmail.com>
2026-09-06 08:11:24 +0100

    Enhance password manager: fix typos, improve user input handling, and add website log functionality

--- pass_manager.py
+++ pass_manager.py
@@ -9,6 +9,8 @@ take a username and a password an duses it to encrpt our password and username a
 import os
 import json
 
+# Application configuration
+
 APP_NAME="Sharry10XY"
 USERS_FILE="users.json"
 WEBSITE_LOGS_FILE="websites_logs.json"
@@ -17,11 +19,14 @@ print("=========================================================================
 print(f"Welcome to {APP_NAME}\nYour passwords are in good hands built by the sharry team")
 print(f"\n")
 
-is_returnign_user = input(f"Are you a returnign user or a new user (r,n):  ")
+# this input checks if the user is a returning user or a new user and if the input is not r or n it will print an error message and quit the application
+is_returning_user = input(f"Are you a returning user or a new user (r,n):  ")
 
-if is_returnign_user not in ["r", "n"]:
+if is_returning_user not in ["r", "n"]:
     print("That seems like an error start the app again")
     quit()
+
+    
 
 # this function checks if a file exist if not it create it and load the file for the applicication to use
 def check_and_load_file(filename):
@@ -31,7 +36,7 @@ def check_and_load_file(filename):
             f.write("[]")
     with open(filename, "r") as f:
         return json.load(f)
-
+# this function checks if a file exist if not it create it and write the data to the file for the applicication to use
 def check_and_write_file(filename, data):
     if not os.path.isfile(filename):
         print("Databases doesn't exits creating now")
@@ -40,11 +45,14 @@ def check_and_write_file(filename, data):
     with open(filename, "w") as f:
         f.write(data)
 
+# this loads the users.json and websites_logs.json files into the users_db and website_logs variables respectively
 users_db = check_and_load_file(USERS_FILE)
+website_logs = check_and_load_file(WEBSITE_LOGS_FILE)
+# this variable will hold the logged in user information after successful login or account creation
 logged_in_user = None
 
-# login logic for returning users 
-if is_returnign_user.lower() == "r":
+# login logic for returning users and new user creation logic for new users
+if is_returning_user.lower() == "r":
     print("its nice to have you back")
     username = input("Enter username: ")
     password = input("Enter Password: ")
@@ -53,7 +61,7 @@ if is_returnign_user.lower() == "r":
         print("Username and password is required")
         quit()
 
-    # this conditon loops through the user_db and checck if the username equals to the username in the user_db and the checks if the password is correct
+    # this conditon loops through the users_db and checks if the username and password match any user in the users_db if it does it sets the logged_in_user to that user and breaks the loop
 
     for user in users_db:
         if (user["username"] != username):
@@ -70,7 +78,8 @@ else:
     print("It would be wonderful to trust us with your password")
     username = input("Enter username: ")
     password = input("Enter Password: ")
-    # this creates a user and adds an id based on the id loop in the user_db
+
+    # this creates a new user and adds it to the users_db and also writes the users_db to the users.json file
     if not username or not password:
         print("Username and password is required")
         quit()
@@ -82,7 +91,69 @@ else:
     }
     users_db.append(user)
 
-     # this function adds new_user and add it back to the user_db
+     # this function checks if the file exists and writes the user_db to the users.json file
     check_and_write_file(USERS_FILE,json.dumps(users_db, indent=4))
     logged_in_user = user
     print("Account created successfully")
+
+
+def view_all_website_logs():
+    print("\nYour saved website accounts:")
+    # user_logs = [log for log in website_logs if log["user_id"] == logged_in_user["id"]]
+
+    user_logs = []
+    for log in website_logs:
+        if log["user_id"] == logged_in_user["id"]:
+            user_logs.append(log)
+
+    if not user_logs:
+        print("No saved website accounts found.")
+        return
+    for log in user_logs:
+        print(f"Website: {log['website']}, Username: {log['username']}, Password: {log['password']}")    
+   
+
+# This function adds a website log to the website_logs.json file with the website, username and password and also adds the user_id to the website log so that we can know which user added the website log 
+    
+def add_website_log(website, website_username, website_password): 
+    log = { "id": len(website_logs) + 1, 
+           "website": website, 
+           "username": website_username, 
+           "password": website_password, 
+# This adds the user_id to the website log so that we can know which user added the website log
+           "user_id": logged_in_user["id"] 
+           }    
+    website_logs.append(log) 
+    check_and_write_file( WEBSITE_LOGS_FILE, json.dumps(website_logs, indent=4) ) 
+    print(f"Website log for {website} added successfully.")
+
+#  this function checks if the user wants to save a website account and if yes it takes the website, username and password and saves it to the website_logs.json file
+
+print("\nDo you want to save a website account?") 
+
+save_website = input("yes/no: ").lower() 
+if save_website == "yes": 
+    website = input("Enter website: ") 
+    website_username = input("Enter website username/email: ") 
+    website_password = input("Enter website password: ") 
+    if not website or not website_username or not website_password:
+        print("Website, username and password is required")
+        quit()
+    add_website_log( website, website_username, website_password )
+else: 
+    get_one_website_log = input("Do you want to view a specific website log? (yes/no): ").lower()
+    if get_one_website_log == "yes":
+        specfic_website_to_retrive = input("Enter website name: ")
+        user_logs = []
+        for log in website_logs:
+            if log["user_id"] == logged_in_user["id"] and log["website"] == specfic_website_to_retrive:
+                user_logs.append(log)
+
+        if len(user_logs) == 0:
+            print(f"No website found for {specfic_website_to_retrive}")
+        for log in user_logs:
+            print(f"Website: {log['website']}, Username: {log['username']}, Password: {log['password']}")   
+
+        pass
+    else:
+        view_all_website_logs()
